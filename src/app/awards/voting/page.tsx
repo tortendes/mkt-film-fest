2b85afe2a686251b5500241387f8ea127b6cd46a
Jarelle Emmanuel Payad Andulan <me@torten.xyz>
2023-04-26 20:24:16 +0800

    Defaulto

--- src/app/awards/voting/page.tsx
+++ src/app/awards/voting/page.tsx
@@ -7,8 +7,6 @@ import { initFirebase } from '../../../../utils/firebase'
 import { getAuth, signInWithPopup, FacebookAuthProvider } from 'firebase/auth'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import Image from 'next/image'
-import { RadioGroup } from '@headlessui/react';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const mt = Montserrat({ subsets: ["latin"] });
@@ -27,19 +25,17 @@ const signIn = async () => {
     }
 }
 
-
 export default function Voting(): JSX.Element {
-    const [user, loading, error] = useAuthState(auth)
-    const { control, handleSubmit, watch, formState: { errors } } = useForm();
-    const done = (data: any) => console.log(data);
+    const [user, loading, error] = useAuthState(auth);
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = (data:any) => console.log(data);
 
-    const [bp, setbp] = useState('')
-    const [bbg, setbbg] = useState('')
+    const test = watch("bgm-select-1")
 
     if (loading) {
         return (
             <main className={`${mt.className} h-screen bg-hero-voting bg-cover bg-no-repeat text-white flex flex-col items-center justify-center`}>
-                <h1 className="text-3xl font-bold tracking-widest">LOADING</h1>
+                <h1 className="text-3xl font-bold tracking-widest text-black">LOADING</h1>
                 <h3 className="text-xl">Please wait...</h3>
             </main>
         )
@@ -66,116 +62,57 @@ export default function Voting(): JSX.Element {
                 <h2 className='text-white'>Signed in as {user.displayName}</h2>
             </div>
             <div className="px-5 mt-7">
-                <form onSubmit={handleSubmit(done)}>
-                    <div className="film-category">
-                        <h1 className="cat-title">
-                            Best Picture (Poster)
-                        </h1>
-                        <h3 className="cat-desc">
-                            The Best Poster award celebrates the artistry and creativity that goes into crafting an unforgettable movie poster. The winning poster captures the essence of the movie with striking visuals, bold typography, and innovative design. It entices audiences with a tantalizing glimpse of the film&#39;s world and promises an unforgettable cinematic experience. Whether it&#39;s a minimalist masterpiece, a breathtaking collage, or a bold and daring design, the Best Poster award showcases the very best of movie marketing and design.
-                        </h3>
-                    </div>
-                        <RadioGroup value={bp} onChange={setbp} className="grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 gap-3 mt-3">
-                            <RadioGroup.Option value="Malikmata">
-                                {({ checked }) => (
-                                    <div className='bg-custom-gold flex items-center justify-evenly gap-3 w-full h-[10em]'>
-                                        <img src="/tw-public/posters/malikmata.png" alt="Hello po!" height="" className='h-full w-auto'  />
-                                        <div className="px-3 py-2 text-white grow">
-                                            <h1 className='uppercase text-center text-2xl'>Malikmata</h1>
-                                            <hr className="bg-gray-200 border-0 text-lg" />
-                                            <h3 className={`${cg.className} text-black text-center`}>Jarelle Emmanuel Payad Andulan</h3>
-                                        </div>
-                                        { checked &&
-                                            <div className="px-8">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/><polyline points="22 4 12 14.01 9 11.01"/></svg>
-                                            </div>
-                                        }
-                                    </div>
-                                )}
-                            </RadioGroup.Option>
-                            <RadioGroup.Option value="Lila">
-                                {({ checked }) => (
-                                    <div className='bg-custom-gold flex items-center justify-evenly gap-3 w-full h-[10em]'>
-                                        <img src="/tw-public/posters/lila.jpg" alt="Hello po!" className='h-full w-auto'  />
-                                        <div className="px-3 py-2 text-white grow">
-                                            <h1 className='uppercase text-center text-2xl'>Lila</h1>
-                                            <hr className="bg-gray-200 border-0 text-lg" />
-                                            <h3 className={`${cg.className} text-black text-center`}>Juno Isaac Jagape, Althea Paglinawan</h3>
-                                        </div>
-                                        { checked &&
-                                            <div className="px-8">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/><polyline points="22 4 12 14.01 9 11.01"/></svg>
-                                            </div>
-                                        }
-                                    </div>
-                                )}
-                            </RadioGroup.Option>
-                            <RadioGroup.Option value="Somnium">
-                                {({ checked }) => (
-                                    <div className='bg-custom-gold flex items-center justify-evenly gap-3 w-full h-[10em]'>
-                                        <img src="/tw-public/posters/somnium.jpg" alt="Hello po!" className='h-full w-auto'  />
-                                        <div className="px-3 py-2 text-white grow">
-                                            <h1 className='uppercase text-center text-2xl'>Somnium</h1>
-                                            <hr className="bg-gray-200 border-0 text-lg" />
-                                            <h3 className={`${cg.className} text-black text-center`}>Juno Isaac Jagape, Althea Paglinawan</h3>
-                                        </div>
-                                        { checked &&
-                                            <div className="px-8">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/><polyline points="22 4 12 14.01 9 11.01"/></svg>
-                                            </div>
-                                        }
-                                    </div>
-                                )}
-                            </RadioGroup.Option>
-                        </RadioGroup>
-
-
-                        <div className="film-category">
-                            <h1 className="cat-title">
-                                Best Background Music
-                            </h1>
-                            <h3 className="cat-desc">
-                                The Best Background Music award acknowledges the important role of music in setting the mood and tone of a short film. This award honors the seamless integration of background music into the storytelling, enhancing the narrative and immersing viewers in the film&quot;s world.
-                            </h3>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="film-category">
+                    <h1 className="cat-title">
+                        Best Picture (Poster)
+                    </h1>
+                    <h3 className="cat-desc">
+                        The Best Poster award celebrates the artistry and creativity that goes into crafting an unforgettable movie poster. The winning poster captures the essence of the movie with striking visuals, bold typography, and innovative design. It entices audiences with a tantalizing glimpse of the film&#39;s world and promises an unforgettable cinematic experience. Whether it&#39;s a minimalist masterpiece, a breathtaking collage, or a bold and daring design, the Best Poster award showcases the very best of movie marketing and design.
+                    </h3>
+                    {/* <CustomRadioGroup
+                        value={null}
+                        onChange={(val) => console.log(val)}
+                        options={[
+                            {
+                                title: "Malikmata",
+                                image: "/tw-public/posters/malikmata.png",
+                                description: "Jarelle Emmanuel Payad Andulan",
+                                value: "bp-malikmata"
+                            }
+                        ]}
+                    /> */}
+                </div>
+                <div className="film-category">
+                    <h1 className="cat-title">
+                        Besst Background Music
+                    </h1>
+                    <h3 className="cat-desc">
+                        The Best Background Music award acknowledges the important role of music in setting the mood and tone of a short film. This award honors the seamless integration of background music into the storytelling, enhancing the narrative and immersing viewers in the film&quot;s world.
+                    </h3>
+                    <fieldset className="flex flex-wrap">
+                        <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                            <div className="bg-custom-gold rounded-sm p-3 shadow-md relative">
+                                <input type="radio" id="bgm-select-1" className="absolute h-0 w-0 appearance-none" checked value="Malikmata" { ...register("bgm-select-1") }/>
+                                <label htmlFor="bgm-select-1" className='hover:cursor-pointer flex flex-col items-center justify-center'>
+                                    <Image src="/tw-public/posters/malikmata.png" alt="Malikmata" className='w-full h-auto object-cover' width={2592} height={3861} />
+                                    <h1 className="text-3xl font-semibold text-white">Malikmata</h1>
+                                </label>
+                            </div>
                         </div>
-                            <RadioGroup value={bbg} onChange={setbbg} className="grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 gap-3 mt-3">
-                                <RadioGroup.Option value="Malikmata">
-                                    {({ checked }) => (
-                                        <div className='bg-custom-gold flex items-center justify-evenly gap-3 w-full h-[10em]'>
-                                            <img src="/tw-public/posters/malikmata.png" alt="Hello po!" height="" className='h-full w-auto'  />
-                                            <div className="px-3 py-2 text-white grow">
-                                                <h1 className='uppercase text-center text-2xl'>Malikmata</h1>
-                                                <hr className="bg-gray-200 border-0 text-lg" />
-                                                <h3 className={`${cg.className} text-black text-center`}>Jarelle Emmanuel Payad Andulan</h3>
-                                            </div>
-                                            { checked &&
-                                                <div className="px-8">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/><polyline points="22 4 12 14.01 9 11.01"/></svg>
-                                                </div>
-                                            }
-                                        </div>
-                                    )}
-                                </RadioGroup.Option>
-                                <RadioGroup.Option value="Lila">
-                                    {({ checked }) => (
-                                        <div className='bg-custom-gold flex items-center justify-evenly gap-3 w-full h-[10em]'>
-                                            <img src="/tw-public/posters/lila.jpg" alt="Hello po!" className='h-full w-auto'  />
-                                            <div className="px-3 py-2 text-white grow">
-                                                <h1 className='uppercase text-center text-2xl'>Lila</h1>
-                                                <hr className="bg-gray-200 border-0 text-lg" />
-                                                <h3 className={`${cg.className} text-black text-center`}>Juno Isaac Jagape, Althea Paglinawan</h3>
-                                            </div>
-                                            { checked &&
-                                                <div className="px-8">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"/><polyline points="22 4 12 14.01 9 11.01"/></svg>
-                                                </div>
-                                            }
-                                        </div>
-                                    )}
-                                </RadioGroup.Option>
-                            </RadioGroup>
-
-                        <button type='submit' className='fixed bottom-4 left-4 bg-[#222] border-2 border-custom-gold text-white px-5 py-2 w-48'>Submit</button>
+                        <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
+                            <div className="bg-custom-gold rounded-sm p-3 shadow-md relative">
+                                <input type="radio" id="bgm-select-1" className="absolute h-0 w-0 appearance-none" { ...register("bgm-select-1") } value="Lila"/>
+                                <label htmlFor="bgm-select-1" className='hover:cursor-pointer flex flex-col items-center justify-center'>
+                                    <Image src="/tw-public/posters/lila.jpg" alt="Lila" className='w-full h-auto object-cover' width={2592} height={3861} />
+                                    <h1 className="text-3xl font-semibold text-white">Lila</h1>
+                                </label>
+                            </div>
+                        </div>
+                    </fieldset>
+                    {test}
+                </div>
+                    <button type='submit' className='fixed bottom-4 left-4 bg-[#222] border-2 border-custom-gold text-white px-5 py-2 w-48'>Submit</button>
                 </form>
             </div>
         </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms')
+    // require('@tailwindcss/forms')
   ],
 }
